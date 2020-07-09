231077232fec149f61055f08a8e0b072744c27e1
saket <saket@nickelfox.com>
2020-07-09 20:10:29 +0530

    Basic complete

---
+++ goroutines.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"sync"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+// Go Channels
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+// Channel Close
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+// Fibonnaci with select
+func fibonacci_select(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+// Mutex
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+// Inc increments the counter for the given key.
+func (c *SafeCounter) Inc(key string) {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.v[key]++
+	c.mux.Unlock()
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeCounter) Value(key string) int {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
+func main() {
+	go say("world")
+	say("hello")
+
+	// Go Channels
+	s := []int{7, 2, 8, -9, 4, 0}
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+	fmt.Println(x, y, x+y)
+
+	// Buffered Channels
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+
+	// Channel Close
+	c1 := make(chan int, 10)
+	go fibonacci(cap(c1), c1)
+	for i := range c1 {
+		fmt.Println("Fibonnaci", i)
+	}
+
+	// swith with default
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+
+	// Mutex
+	c2 := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c2.Inc("somekey")
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("Mutex ", c2.Value("somekey"))
+}
